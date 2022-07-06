3d6e99910f4238aa10df1238f43290a1c063ac18
maxgunchoi <chleorjs37@gmail.com>
2022-07-07 08:39:52 +0900

    반응형

--- js/slick.js
+++ js/slick.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+    //slick-slider
+    $('.slick-slider').slick({
+        autoplay:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        fade: true,
+        pauseOnHover: false,
+        cssEase: 'linear'
+    })
 
 })
