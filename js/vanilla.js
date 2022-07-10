eb9c7d1f0e36aea601aed3c660ff5e88bf6f06a9
CHOI DAE GEON <chleorjs37@gmail.com>
2022-07-10 17:59:56 +0900

    진행중

--- js/vanilla.js
+++ js/vanilla.js
@@ -2,10 +2,13 @@ const hambugerEvent = () =>{
     const hamEl = document.getElementById('hambuger');
     hamEl.addEventListener('click',()=>{
         if(hamEl.classList.contains('active')){
-            hamEl.classList.remove('active')
+            hamEl.classList.remove('active');
+            overlay.setAttribute('style','display:none;');
         }
         else{
-            hamEl.classList.add('active')
+            hamEl.classList.add('active');
+            const overlay = document.getElementById('overlay');
+            overlay.setAttribute('style','display:inline-block;');
         }
         
     })
