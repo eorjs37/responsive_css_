59d98456e8ccf6c11aa077c49397f2df810c56ea
CHOI DAE GEON <chleorjs37@gmail.com>
2022-07-30 18:34:33 +0900

    햄버가 바

--- js/vanilla.js
+++ js/vanilla.js
@@ -1,19 +1,21 @@
-const hambugerEvent = () =>{
-    const hamEl = document.getElementById('hambuger');
-    hamEl.addEventListener('click',()=>{
-        if(hamEl.classList.contains('active')){
-            hamEl.classList.remove('active');
-            overlay.setAttribute('style','display:none;');
-        }
-        else{
-            hamEl.classList.add('active');
-            const overlay = document.getElementById('overlay');
-            overlay.setAttribute('style','display:inline-block;');
-        }
-        
-    })
-}
+const hambugerEvent = () => {
+  const hamEl = document.getElementById("hambuger");
+  const sideBar = document.getElementsByClassName("side-bar");
+  //const sideBarWidth = sideBar[0].clientWidth();
+  hamEl.addEventListener("click", () => {
+    if (hamEl.classList.contains("active")) {
+      hamEl.classList.remove("active");
+      overlay.setAttribute("style", "display:none;");
+      sideBar[0].setAttribute("style", `left:-225px;`);
+    } else {
+      hamEl.classList.add("active");
+      const overlay = document.getElementById("overlay");
+      overlay.setAttribute("style", "display:inline-block;");
+      sideBar[0].setAttribute("style", "left:0px;");
+    }
+  });
+};
 
-window.onload = () =>{
-    hambugerEvent();
-}
+window.onload = () => {
+  hambugerEvent();
+};
