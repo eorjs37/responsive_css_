411e921d300a1bbf397b783f697d631e39e8a845
CHOI DAE GEON <chleorjs37@gmail.com>
2022-07-31 12:29:22 +0900

    header 완료

--- js/vanilla.js
+++ js/vanilla.js
@@ -1,12 +1,11 @@
 const hambugerEvent = () => {
   const hamEl = document.getElementById("hambuger");
   const sideBar = document.getElementsByClassName("side-bar");
-  //const sideBarWidth = sideBar[0].clientWidth();
   hamEl.addEventListener("click", () => {
     if (hamEl.classList.contains("active")) {
       hamEl.classList.remove("active");
       overlay.setAttribute("style", "display:none;");
-      sideBar[0].setAttribute("style", `left:-225px;`);
+      sideBar[0].setAttribute("style", `left:-255px;`);
     } else {
       hamEl.classList.add("active");
       const overlay = document.getElementById("overlay");
