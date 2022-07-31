663045fafe79f181a5be2f86a7f266d2464c3663
CHOI DAE GEON <chleorjs37@gmail.com>
2022-07-31 20:45:35 +0900

    header scroll event 처리

--- js/vanilla.js
+++ js/vanilla.js
@@ -18,7 +18,20 @@ const typeIt = () => {
   new TypeIt(".type-effect").type("Business strategy.").delete().pause(1000).type("Innovation Plan.").delete().pause(1000).type("Creative Idea.").go();
 };
 
+const boxShadow = () => {
+  const header = document.getElementById("header");
+  if (window.scrollY > 0) {
+    console.log(header);
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
+
 window.onload = () => {
   hambugerEvent();
   typeIt();
+
+  //scroll 감지
+  window.addEventListener("scroll", boxShadow);
 };
